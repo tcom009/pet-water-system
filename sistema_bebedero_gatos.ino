dfcb1b3c3d4015b6a94b20da1b8741eb7c816d0a
Gerson Mayer <g.mayerduran@gmail.com>
2020-01-07 18:47:36 -0500

    definicion provisional de variables

--- sistema_bebedero_gatos.ino
+++ sistema_bebedero_gatos.ino
@@ -1,11 +1,51 @@
+//falta importar la libreria lcdi2c
+
+ 
+
+
+
+const int motorGrande =0;
+const int motorPlato =0;
+const int sensorFlujo =0;
+const int sensorNivel =0;
+const int sensorMovimiento=0;
+const int valvulaCarga =0;
+const int valvulaDrenado =0;
 
 
 void setup() {
-  // put your setup code here, to run once:
+
+
+
+  pinMode (motorGrande, OUTPUT);
+  pinMode(motorPlato, OUTPUT);
+  pinMode(valvulaCarga, OUTPUT);
+  pinMode(valvulaDrenado, OUTPUT);
+  pinMode (sensorFlujo,INPUT);
+  pinMode (sensorNivel,INPUT);
+  pinMode (sensorMovimiento,INPUT);
+  Serial.begin (9600);
+}
+
+void cargarAgua(){
 
 }
 
+void drenar(){
+
+}
+
+void chorro(){
+
+}
+
+void calcularHora(){
+
+}
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+
 
 }
